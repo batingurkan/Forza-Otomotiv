0ac7bad1ce8a2289caf58848245a38f0fbb48e7b
Batın Gürkan <batingurkan01@gmail.com>
2023-11-02 22:27:20 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using Forza_Otomotiv.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forza_Otomotiv.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+        public IActionResult Hakkımızda()
+        {
+            return View();
+        }
+
+        public IActionResult Tedarikcilerimiz()
+            {
+                return View();
+            }
+        public IActionResult İletisim()
+            {
+                return View();
+            }
+        public IActionResult Bayibasvuru()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Forza_Otomotiv.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
